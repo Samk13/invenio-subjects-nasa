c55ff4fbd197f41343f3893ef3e23f96977499e2
Sam AL-Arbid <samaa2@kth.se>
2022-08-20 17:24:57 +0200

    remove unused functionality

--- invenio_subjects_nasa/load_data.py
+++ invenio_subjects_nasa/load_data.py
@@ -1,38 +1,16 @@
 """Load raw csv file and clean it up."""
-import csv
-
-from invenio_subjects_nasa.utils import logger
+import ast
 
 
 def load_data(raw_csv_data):
     """Load data.
 
     Args:
-        row_data (csv): row csv file coming from
+        row_data (csv): row csv file downloaded from
         https://www.sti.nasa.gov/nasa-thesaurus/
     """
     with open(raw_csv_data, newline="\n", encoding="utf-8") as raw_csv:
-        f = csv.reader(raw_csv)
         # skip header
-        next(f, None)
-        for row in f:
-            logger(row)
-            yield clean_row(row)
-
-
-def write_data(clean_csv, file_name):
-    """Write data to csv file.
-
-    Args:
-        clean_csv (list): csv row list
-        file_name (string): output filename
-    """
-    with open(file_name, "w", encoding="utf-8") as yaml_f:
-        for row in clean_csv:
-            print(row, file=yaml_f)
-
-
-def clean_row(r):
-    """Strip out unwanted chars."""
-    for index in r:
-        return index.replace('"', "").split(",")
+        next(raw_csv, None)
+        for row in raw_csv:
+            yield ast.literal_eval(row).split(',')
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import click
 
 from invenio_subjects_nasa.converter import csv_to_yaml, write_to_disk
 from invenio_subjects_nasa.load_data import load_data
+from invenio_subjects_nasa.utils import logger
 
 row_data = "invenio_subjects_nasa/downloads/thesaurus-CSV.csv"
 output_yaml = "invenio_subjects_nasa/vocabularies/nasa_thesaurus.yaml"
@@ -10,11 +11,12 @@ output_yaml = "invenio_subjects_nasa/vocabularies/nasa_thesaurus.yaml"
 @click.command()
 def main():
     """initiate the app"""
+    logger("Converting ...")
     res = []
     for row in load_data(row_data):
         res.append(csv_to_yaml(row))
     write_to_disk(res, output_yaml)
-
+    logger("Subjects is been converted successfully!")
 
 if __name__ == "__main__":
     main()
